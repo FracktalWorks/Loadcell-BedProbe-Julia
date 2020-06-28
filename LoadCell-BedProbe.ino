cc89cb1d6cd63ca08e333aee79b19f563de709ec
Vijay Raghav Varada <vjvarada@yahoo.co.in>
2020-06-28 19:32:11 +0530

    updated code with doccumentation

--- LoadCell-BedProbe.ino
+++ LoadCell-BedProbe.ino
@@ -25,10 +25,11 @@ bool dir, prevDir;
 #define SETTLE_TIME 500
 #define LED_PIN 6
 #define INVERT_Z_DIR true  //false if Z_DIR is HIGH when moving towards bed.
+#define DEBUG 1 //switch serial output
 
-#define DEBUG 1
-
-
+//-------------------------------------------------------------------------------------
+// TARE's the loadcell value and resets all outputs
+//-------------------------------------------------------------------------------------
 void tareLoadCell()
 {
   #ifdef LED_PIN
@@ -45,32 +46,36 @@ void tareLoadCell()
   #endif
 
 }
-
+//-------------------------------------------------------------------------------------
+// Function that returns if the bed is moving towards wht nozzle ot not
+//-------------------------------------------------------------------------------------
 bool isMovingTowardBed()
 {
-#if INVERT_Z_DIR == true
-    if (digitalRead(DIR_PIN))
-    {
+  #if INVERT_Z_DIR == true
+      if (digitalRead(DIR_PIN))
+      {
 
-      return false; //Moving towards from bed
-    }
-    else
-    {
-      return true; //moving away from bed
-#else
-   if (digitalRead(DIR_PIN))
-    {
+        return false; //Moving towards from bed
+      }
+      else
+      {
+        return true; //moving away from bed
+  #else
+     if (digitalRead(DIR_PIN))
+      {
 
-      return true; //Moving towards from bed
+        return true; //Moving towards from bed
+      }
+      else
+      {
+        return false; //moving away from bed
+  #endif
     }
-    else
-    {
-      return false; //moving away from bed
-#endif
-  }
 }
-
-bool isChangedDirectionTowardBed() //function to check if bed changed direction towards nozzle
+//-------------------------------------------------------------------------------------
+// Function to check if bed just changed direction towards nozzle
+//-------------------------------------------------------------------------------------
+bool isChangedDirectionTowardBed() //
 {
   dir = digitalRead(DIR_PIN);
   #if INVERT_Z_DIR == true
@@ -93,7 +98,9 @@ bool isChangedDirectionTowardBed() //function to check if bed changed direction 
     return false;
   }
 }
-
+//-------------------------------------------------------------------------------------
+// Setup fucntion
+//-------------------------------------------------------------------------------------
 void setup()
 {
   #ifdef DEBUG
@@ -117,6 +124,9 @@ void setup()
 
 }
 
+//-------------------------------------------------------------------------------------
+// Main Loop
+//-------------------------------------------------------------------------------------
 void loop()
 {
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Loadcell-BedProbe-Julia
-Code for Arduino nano ABL for Julia series of printers ( Extended, Advanced, Classis)
+# loadCellProbe
+
+## Arduino Nano Code for loadcell automatic bed leveling for Julia series ( Extended, Advanced, Classic) of 3D Printers with uses 3 loadcells under bed
+
+### Usage:
+1. Set INVERT_Z_DIR so select in which Z Direction the loadcell amp should be giving output
+2. LOWERTHRESHOLD UPPERTHRESHOLD set the upper and lower thresholds for the amplifier to just trigger, and start blinking LED
